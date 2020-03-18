11edd249d84b5cc431ff011a940756b695ed7f12
RexTheWho <jrcrex1@gmail.com>
2020-03-18 20:45:51 +0800

    upd

--- hooks/ZM_ContractHudManager.lua
+++ hooks/ZM_ContractHudManager.lua
@@ -2,7 +2,8 @@ Hooks:PostHook(ContractBrokerHeistItem, "init", "zm_init_contract_eeicon", funct
     local job_tweak = tweak_data.narrative:job_data(self._job_data.job_id)
 
 	--WIP EE GUI
-    if job_tweak.contact == "zombies_modeA" then
+	--[[
+    if job_tweak.contact == "zombies_mode" then
         if self._image_panel then
 		
 			local ee_completed = Global.mission_manager.saved_job_values[key] == 1
@@ -38,5 +39,5 @@ Hooks:PostHook(ContractBrokerHeistItem, "init", "zm_init_contract_eeicon", funct
 			})
         end
     end
-
+	]]--
 end)
